a9580d512cb83f83392154aafc632a34c5a888b2
Maria Emad <149503796+MARIA-EMAD-99@users.noreply.github.com>
2024-03-12 21:05:02 +0200

    Add files via upload

---
+++ phone project/main.js
@@ -0,0 +1,10 @@
+let imge = document.querySelector('.imges')
+let container = document.querySelector('.container')
+
+function phones(ele){
+    imge.src = ele
+}
+
+function background(color) {
+    container.style.background= color
+}
